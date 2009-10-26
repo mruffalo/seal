2d244a1824de3a377cfe36ec56521f546ecb0f75
Matthew Ruffalo <matthew.ruffalo@case.edu>
2009-10-26 19:25:43 -0400

    Override equals() and hashCode() for Fragment.
    This is in preparation for annotating a Fragment with its assembled position.

--- src/assembly/Fragment.java
+++ src/assembly/Fragment.java
@@ -25,6 +25,44 @@ public class Fragment
 	}
 	
 	@Override
+	public int hashCode()
+	{
+		return string.hashCode();
+	}
+	
+	/**
+	 * <code>equals()</code> can be more strict than <code>hashCode()</code> without violating the
+	 * contract of these two methods. TODO: Examine this more thoroughly
+	 */
+	@Override
+	public boolean equals(Object o)
+	{
+		if (!(o instanceof Fragment))
+		{
+			return false;
+		}
+		Fragment that = (Fragment) o;
+		if (!string.equals(that.string))
+		{
+			return false;
+		}
+		for (FragmentPositionSource source : FragmentPositionSource.values())
+		{
+			Integer thisPosition = this.getPosition(source);
+			Integer thatPosition = that.getPosition(source);
+			/*
+			 * Only compare positions that are present in both fragments. If a position has not been
+			 * assigned in one of the fragments, it doesn't count toward equality.
+			 */
+			if (thisPosition != null && thatPosition != null && thisPosition != thatPosition)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
 	public String toString()
 	{
 		return string;
