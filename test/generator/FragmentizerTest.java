5a793000c6c105ac2c5091061fbf43853516ee19
Matthew Ruffalo <matthew.ruffalo@case.edu>
2009-11-09 18:29:09 -0500

    Begin a Fragmentizer test

---
+++ test/generator/FragmentizerTest.java
@@ -0,0 +1,40 @@
+package generator;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FragmentizerTest
+{
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+	
+	/**
+	 * TODO
+	 */
+	@Test
+	public void testFragmentizeForShotgun()
+	{
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * TODO
+	 */
+	@Test
+	public void testRemoveSubstrings()
+	{
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * TODO
+	 */
+	@Test
+	public void testGroupByLine()
+	{
+		fail("Not yet implemented");
+	}
+}
