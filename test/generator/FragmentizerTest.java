06890cc99b0df59ebcf47aafc5b7545a77c76f09
Matthew Ruffalo <matthew.ruffalo@case.edu>
2009-11-09 19:08:15 -0500

    FragmentizerTest: implement testRemoveSubstrings

--- test/generator/FragmentizerTest.java
+++ test/generator/FragmentizerTest.java
@@ -1,14 +1,33 @@
 package generator;
 
 import static org.junit.Assert.*;
+import java.util.*;
 import org.junit.Before;
 import org.junit.Test;
+import assembly.Fragment;
 
 public class FragmentizerTest
 {
+	private final String[] stringsWithSubstrings = { "ABCDEFG", "BCDEF", "DEFGH", "EFG", "FGHIJK", "K" };
+	private Set<String> stringsWithoutSubstrings;
+	private List<Fragment> listWithSubstrings;
+	
 	@Before
 	public void setUp() throws Exception
 	{
+		List<Fragment> temp = new LinkedList<Fragment>();
+		for (String string : stringsWithSubstrings)
+		{
+			temp.add(new Fragment(string));
+		}
+		listWithSubstrings = Collections.unmodifiableList(temp);
+		
+		Set<String> tempSet = new HashSet<String>();
+		for (String string : new String[] { "ABCDEFG", "DEFGH", "FGHIJK" })
+		{
+			tempSet.add(string);
+		}
+		stringsWithoutSubstrings = Collections.unmodifiableSet(tempSet);
 	}
 	
 	/**
@@ -26,7 +45,18 @@ public class FragmentizerTest
 	@Test
 	public void testRemoveSubstrings()
 	{
-		fail("Not yet implemented");
+		Set<String> strings = new HashSet<String>();
+		List<Fragment> fragments = Fragmentizer.removeSubstrings(listWithSubstrings);
+		for (Fragment f : fragments)
+		{
+			strings.add(f.string);
+		}
+		// Sanity check: make sure that we don't get any duplicate strings
+		assertEquals(strings.size(), fragments.size());
+		for (String s : strings)
+		{
+			assertTrue(stringsWithoutSubstrings.contains(s));
+		}
 	}
 	
 	/**
