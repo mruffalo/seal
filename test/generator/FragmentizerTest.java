ea4e3700470114f6e00e80d4b7b1512a3b3953f8
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-01-14 12:00:08 -0500

    Add test for removing duplicate strings with positions

--- test/generator/FragmentizerTest.java
+++ test/generator/FragmentizerTest.java
@@ -5,12 +5,18 @@ import java.util.*;
 import org.junit.Before;
 import org.junit.Test;
 import assembly.Fragment;
+import assembly.FragmentPositionSource;
 
 public class FragmentizerTest
 {
-	private final String[] stringsWithSubstrings = { "ABCDEFG", "BCDEF", "DEFGH", "EFG", "FGHIJK", "K" };
+	private final String[] stringsWithSubstrings = { "ABCDEFG", "BCDEF", "DEFGH", "EFG", "ABCDEFG", "FGHIJK", "K" };
 	private Set<String> stringsWithoutSubstrings;
 	private List<Fragment> listWithSubstrings;
+	
+	private final String duplicateString = "TCTCACTAAGACGGACAACC";
+	private final int origPos = 148;
+	private final int assembledPos = 247;
+	private List<Fragment> duplicatesWithPositions;
 	
 	@Before
 	public void setUp() throws Exception
@@ -28,6 +34,17 @@ public class FragmentizerTest
 			tempSet.add(string);
 		}
 		stringsWithoutSubstrings = Collections.unmodifiableSet(tempSet);
+		
+		temp = new LinkedList<Fragment>();
+		Fragment f = new Fragment(duplicateString);
+		f.setPosition(FragmentPositionSource.ORIGINAL_SEQUENCE, origPos);
+		f.setPosition(FragmentPositionSource.ASSEMBLED_SEQUENCE, origPos);
+		temp.add(f);
+		f = new Fragment(duplicateString);
+		f.setPosition(FragmentPositionSource.ORIGINAL_SEQUENCE, origPos);
+		f.setPosition(FragmentPositionSource.ASSEMBLED_SEQUENCE, assembledPos);
+		temp.add(f);
+		duplicatesWithPositions = Collections.unmodifiableList(temp);
 	}
 	
 	/**
@@ -39,9 +56,6 @@ public class FragmentizerTest
 		fail("Not yet implemented");
 	}
 	
-	/**
-	 * TODO
-	 */
 	@Test
 	public void testRemoveSubstrings()
 	{
@@ -57,6 +71,13 @@ public class FragmentizerTest
 		{
 			assertTrue(stringsWithoutSubstrings.contains(s));
 		}
+	}
+	
+	@Test
+	public void testRemoveSubstringsWithPositions()
+	{
+		List<Fragment> fragments = Fragmentizer.removeSubstrings(duplicatesWithPositions);
+		assertEquals(1, fragments.size());
 	}
 	
 	/**
