aba3461ac80e6a5cbcdf2ab4d0e41a3e5d1488b7
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-01-26 00:30:14 -0500

    More stuff in the About box

--- src/gui/AboutFrame.java
+++ src/gui/AboutFrame.java
@@ -4,6 +4,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class AboutFrame extends JFrame
 {
@@ -12,7 +13,8 @@ public class AboutFrame extends JFrame
 	/**
 	 * TODO: Move this somewhere else
 	 */
-	public final static String SOFTWARE_VERSION = "1.0";
+	public static final String SOFTWARE_VERSION = "1.0";
+	public static final String GITHUB_URL = "http://github.com/mruffalo/fragment-assembly/";
 	
 	private final FragmentDisplay fragmentDisplay;
 	private JPanel panel;
@@ -28,6 +30,10 @@ public class AboutFrame extends JFrame
 		panel.add(new JLabel("Fragment Assembly GUI"));
 		panel.add(new JLabel(String.format("Version %s", SOFTWARE_VERSION)));
 		panel.add(new JLabel("Author: Matthew Ruffalo <matthew.ruffalo@case.edu>"));
+		panel.add(new JLabel("GitHub project page:"));
+		JTextField github = new JTextField(GITHUB_URL);
+		github.setEditable(false);
+		panel.add(github);
 		this.add(panel);
 		
 		this.setLocationRelativeTo(fragmentDisplay.frame);
