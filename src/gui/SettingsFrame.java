d65a1fd971825ae5f3a64406db94c2449fe614b5
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-02-10 00:50:11 -0500

    Show colors in settings frame

--- src/gui/SettingsFrame.java
+++ src/gui/SettingsFrame.java
@@ -20,20 +20,75 @@ public class SettingsFrame extends JFrame
 		fragmentDisplay = fragmentDisplay_;
 		FragmentDisplaySettings settings = fragmentDisplay.getSettings().clone();
 		panel = new JPanel();
-		
 		panel.setLayout(new GridBagLayout());
+		
 		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weighty = 0.1;
+		gbc.weightx = 0.8;
 		JLabel backgroundColorText = new JLabel("Background Color");
-		panel.add(backgroundColorText);
+		panel.add(backgroundColorText, gbc);
 		
 		gbc.gridx = 1;
+		gbc.weightx = 0.2;
 		JLabel backgroundColor = new JLabel();
 		backgroundColor.setOpaque(true);
 		backgroundColor.setBackground(settings.backgroundColor);
 		Dimension sixteen = new Dimension(16, 16);
 		backgroundColor.setMinimumSize(sixteen);
 		backgroundColor.setPreferredSize(sixteen);
-		panel.add(backgroundColor);
+		panel.add(backgroundColor, gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weighty = 0.1;
+		gbc.weightx = 0.8;
+		gbc.gridy = 1;
+		JLabel sequenceColorText = new JLabel("Sequence Color");
+		panel.add(sequenceColorText, gbc);
+		
+		gbc.gridx = 1;
+		gbc.weightx = 0.2;
+		JLabel sequenceColor = new JLabel();
+		sequenceColor.setOpaque(true);
+		sequenceColor.setBackground(settings.sequenceColor);
+		sequenceColor.setMinimumSize(sixteen);
+		sequenceColor.setPreferredSize(sixteen);
+		panel.add(sequenceColor, gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weighty = 0.1;
+		gbc.weightx = 0.8;
+		gbc.gridy = 2;
+		JLabel fragmentColorText = new JLabel("Fragment Color");
+		panel.add(fragmentColorText, gbc);
+		
+		gbc.gridx = 1;
+		gbc.weightx = 0.2;
+		JLabel fragmentColor = new JLabel();
+		fragmentColor.setOpaque(true);
+		fragmentColor.setBackground(settings.fragmentColor);
+		fragmentColor.setMinimumSize(sixteen);
+		fragmentColor.setPreferredSize(sixteen);
+		panel.add(fragmentColor, gbc);
+		
+		gbc = new GridBagConstraints();
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.weighty = 0.1;
+		gbc.weightx = 0.8;
+		gbc.gridy = 3;
+		JLabel selectedColorText = new JLabel("Selected Color");
+		panel.add(selectedColorText, gbc);
+		
+		gbc.gridx = 1;
+		gbc.weightx = 0.2;
+		JLabel selectedColor = new JLabel();
+		selectedColor.setOpaque(true);
+		selectedColor.setBackground(settings.selectedColor);
+		selectedColor.setMinimumSize(sixteen);
+		selectedColor.setPreferredSize(sixteen);
+		panel.add(selectedColor, gbc);
 		
 		this.add(panel);
 		this.setLocationRelativeTo(fragmentDisplay.frame);
