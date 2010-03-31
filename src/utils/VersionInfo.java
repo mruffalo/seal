d0beadad47c4c17833f6f1d0c62abd1462a6d00e
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-03-30 21:39:13 -0400

    Fix NPE in about box if version.properties is missing

--- src/utils/VersionInfo.java
+++ src/utils/VersionInfo.java
@@ -7,20 +7,24 @@ import java.util.Properties;
 public class VersionInfo
 {
 	public static final String SOFTWARE_VERSION;
+	private static final String UNKNOWN_VERSION = "Unknown";
 	
 	static
 	{
 		InputStream istream = VersionInfo.class.getResourceAsStream("version.properties");
 		Properties p = new Properties();
-		String version;
-		try
+		String version = UNKNOWN_VERSION;
+		if (istream != null)
 		{
-			p.load(istream);
-			version = p.getProperty("software.version");
-		}
-		catch (IOException e)
-		{
-			version = "Unknown";
+			try
+			{
+				p.load(istream);
+				version = p.getProperty("software.version", version);
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
 		SOFTWARE_VERSION = version;
 	}
