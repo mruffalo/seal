f66fa0a98d2135c7ce4c2281b643b5e6b07a248f
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-06-17 12:42:14 -0400

    Add skeletal PairedEndFragment code

---
+++ src/assembly/PairedEndFragment.java
@@ -0,0 +1,10 @@
+package assembly;
+
+public class PairedEndFragment extends Fragment
+{
+	public PairedEndFragment(String string)
+	{
+		super(string);
+		// TODO Auto-generated constructor stub
+	}
+}
