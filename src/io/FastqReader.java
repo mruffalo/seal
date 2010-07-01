33cc3e545a84e3993ce622e0553f3bf66cf5c2ff
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-01 14:14:25 -0400

    Add high-level operation commments

--- src/io/FastqReader.java
+++ src/io/FastqReader.java
@@ -1,5 +1,13 @@
 package io;
 
+/**
+ * TODO: Determine how these classes should work at a high level. Perhaps similar to a
+ * BufferedReader: instantiate with another Reader and provide a getFragment method? It probably
+ * makes sense for this class to use a BufferedReader; its {@link java.io.BufferedReader#readLine()}
+ * method isn't worth reinventing.
+ * 
+ * @author mruffalo
+ */
 public class FastqReader
 {
 	private static enum State
