7c4d56c232295de6b8ff512a344ddf9c6ef0eef5
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-13 11:58:48 -0400

    Start to fill in code for BWA execution

--- src/external/BwaInterface.java
+++ src/external/BwaInterface.java
@@ -1,11 +1,18 @@
 package external;
 
+import generator.SeqGenSingleSequenceMultipleRepeats;
+import generator.SequenceGenerator;
+import io.FastaWriter;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import assembly.Fragment;
 
 public class BwaInterface
 {
+	public static final String BWA_COMMAND = "bwa";
+	public static final String INDEX_COMMAND = "index";
+	
 	private String string;
 	private List<Fragment> fragments;
 	
@@ -17,7 +24,17 @@ public class BwaInterface
 	
 	public void createIndex(File file)
 	{
-		
+		ProcessBuilder pb = new ProcessBuilder(BWA_COMMAND, INDEX_COMMAND, file.getAbsolutePath());
+		try
+		{
+			FastaWriter.writeSequence(string, file);
+			Process p = pb.start();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void align()
@@ -28,5 +45,12 @@ public class BwaInterface
 	public void readAlignment()
 	{
 		
+	}
+	
+	public static void main(String args[])
+	{
+		SequenceGenerator g = new SeqGenSingleSequenceMultipleRepeats();
+		String string = g.generateSequence(100000, 0, 0);
+		System.out.println(string);
 	}
 }
