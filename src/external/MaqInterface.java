6057b68d4a735ddd5261c4634aaec1bfb3629507
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-22 12:29:21 -0400

    Add empty MaqInterface

---
+++ src/external/MaqInterface.java
@@ -0,0 +1,28 @@
+package external;
+
+public class MaqInterface extends AlignmentToolInterface
+{
+	@Override
+	public void align()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void postAlignmentProcessing()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void preAlignmentProcessing()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void readAlignment()
+	{
+		// TODO Auto-generated method stub
+	}
+}
