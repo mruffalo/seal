7357bcefe8c545b88c2b74b61031fa997f45d2e3
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-22 15:36:17 -0400

    Add two (empty) FragmentErrorGenerator implementations

---
+++ src/generator/GaussianUniformErrorGenerator.java
@@ -0,0 +1,13 @@
+package generator;
+
+import java.util.List;
+import assembly.Fragment;
+
+public class GaussianUniformErrorGenerator implements FragmentErrorGenerator
+{
+	@Override
+	public void generateErrors(List<Fragment> fragments, String allowedCharacters)
+	{
+		// TODO Auto-generated method stub
+	}
+}
