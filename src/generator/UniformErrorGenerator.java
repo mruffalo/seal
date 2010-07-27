853c3ebddfc1d893fab9cefb8f42b735f269cb34
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-27 12:38:25 -0400

    UniformErrorGenerator: add error probability

--- src/generator/UniformErrorGenerator.java
+++ src/generator/UniformErrorGenerator.java
@@ -5,6 +5,26 @@ import assembly.Fragment;
 
 public class UniformErrorGenerator implements FragmentErrorGenerator
 {
+	private double errorProbability;
+	
+	public void setErrorProbability(double errorProbability_)
+	{
+		if (errorProbability_ <= 1.0 && errorProbability_ >= 0.0)
+		{
+			errorProbability = errorProbability_;
+		}
+		else
+		{
+			// TODO: be nicer about this :)
+			throw new IllegalArgumentException("error probability must be >= 0.0 and <= 1.0");
+		}
+	}
+	
+	public double getErrorProbability()
+	{
+		return errorProbability;
+	}
+	
 	@Override
 	public List<? extends Fragment> generateErrors(List<? extends Fragment> fragments,
 		String allowedCharacters)
