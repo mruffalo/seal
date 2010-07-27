fe481d6f754b91fc88ccd41aac1ed5337c4620e5
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-27 16:08:36 -0400

    Add FASTQ quality characters, constants, beginning of 'write' method

--- src/io/Constants.java
+++ src/io/Constants.java
@@ -6,4 +6,9 @@ public class Constants
 {
 	public static final String FIELD_SEPARATOR = ",";
 	public static final Pattern READ_POSITION_HEADER = Pattern.compile("[>@]\\d+:READ_POS=(\\d+(,\\d+)?)");
+	public static final String FASTQ_QUALITY_CHARACTERS = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	public static final int SANGER_OFFSET = 0;
+	public static final int SOLEXA_OFFSET = 26;
+	public static final int ILLUMINA_1_3_OFFSET = 33;
+	public static final int ILLUMINA_1_5_OFFSET = 36;
 }
--- src/io/FastqWriter.java
+++ src/io/FastqWriter.java
@@ -1,6 +1,54 @@
 package io;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import assembly.Fragment;
+import assembly.FragmentPositionSource;
+
 public class FastqWriter
 {
 	
+	public static void writeFragments(List<? extends Fragment> fragments, File file)
+		throws IOException
+	{
+		BufferedWriter output = null;
+		try
+		{
+			output = new BufferedWriter(new FileWriter(file));
+			int i = 0;
+			for (Fragment fragment : fragments)
+			{
+				StringBuilder fragmentIdentifier = new StringBuilder();
+				fragmentIdentifier.append("FRAGMENT_");
+				fragmentIdentifier.append(i++);
+				Integer originalPosition = fragment.getPosition(FragmentPositionSource.ORIGINAL_SEQUENCE);
+				if (originalPosition != null)
+				{
+					fragmentIdentifier.append(":");
+					fragmentIdentifier.append(originalPosition);
+				}
+				Integer assembledPosition = fragment.getPosition(FragmentPositionSource.ASSEMBLED_SEQUENCE);
+				if (assembledPosition != null)
+				{
+					fragmentIdentifier.append(":");
+					fragmentIdentifier.append(assembledPosition);
+				}
+				output.write(String.format("@%s%n", fragmentIdentifier.toString()));
+				// TODO: Improve this to be Rope-smart
+				output.write(fragment.toString());
+				output.write(String.format("%n"));
+				output.write(String.format("+%s%n", fragmentIdentifier.toString()));
+			}
+		}
+		finally
+		{
+			if (output != null)
+			{
+				output.close();
+			}
+		}
+	}
 }
