92ade830c4ff67ff6d5c31086742ce054a9d0c92
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-07-29 13:15:27 -0400

    FastqWriter: print quality characters

--- src/io/FastqWriter.java
+++ src/io/FastqWriter.java
@@ -10,6 +10,19 @@ import assembly.FragmentPositionSource;
 
 public class FastqWriter
 {
+	private static char getQualityCharacter(int offset, int value)
+	{
+		return Constants.FASTQ_QUALITY_CHARACTERS.charAt(offset + value);
+	}
+
+	/**
+	 * TODO: Improve this API in the same style as the unused methods in
+	 * {@link FastaWriter}
+	 * 
+	 * @param fragments
+	 * @param file
+	 * @throws IOException
+	 */
 	public static void writeFragments(List<? extends Fragment> fragments, File file)
 		throws IOException
 	{
@@ -40,6 +53,15 @@ public class FastqWriter
 				output.write(fragment.toString());
 				output.write(String.format("%n"));
 				output.write(String.format("+%s%n", fragmentIdentifier.toString()));
+				StringBuilder sb = new StringBuilder();
+				for (int j = 0; j < fragment.toString().length(); j++)
+				{
+					// TODO: Un-hardcode Illumina 1.3 settings
+					sb.append(getQualityCharacter(Constants.ILLUMINA_1_3_OFFSET,
+						fragment.getReadQuality(j)));
+				}
+				output.write(sb.toString());
+				output.write(String.format("%n"));
 			}
 		}
 		finally
