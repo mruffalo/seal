0223326987d59127022bea6ce05a5f231149be41
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-10-08 14:50:33 -0400

    Progress in BowtieInterface

--- src/external/BowtieInterface.java
+++ src/external/BowtieInterface.java
@@ -17,6 +17,9 @@ public class BowtieInterface extends AlignmentToolInterface
 {
 	private static final String BOWTIE_COMMAND = "bowtie";
 	private static final String BOWTIE_INDEX_COMMAND = "bowtie-build";
+	private static final String FIRST_PAIR_OPTION = "-1";
+	private static final String SECOND_PAIR_OPTION = "-2";
+	private static final String SAM_OPTION = "--sam";
 
 	public BowtieInterface(int index_, String description_, List<Integer> thresholds_,
 		CharSequence sequence_, List<? extends Fragment> list_, Options o_,
@@ -28,9 +31,11 @@ public class BowtieInterface extends AlignmentToolInterface
 	private void createIndex()
 	{
 		String index_base_name = "index";
+		o.index = new File(o.genome.getParentFile(), index_base_name);
 		String index_addition = ".1.ebwt";
-		o.index = new File(o.genome.getParentFile(), index_base_name + index_addition);
-		if (o.index.isFile())
+		File index_file_to_check = new File(o.genome.getParentFile(), index_base_name
+				+ index_addition);
+		if (index_file_to_check.isFile())
 		{
 			System.out.printf("%03d: %s%n", index, "Index found; skipping");
 		}
@@ -80,13 +85,51 @@ public class BowtieInterface extends AlignmentToolInterface
 	@Override
 	public void align()
 	{
-		// TODO Auto-generated method stub
+		List<String> commands = new ArrayList<String>();
+		commands.add(BOWTIE_COMMAND);
+		commands.add(o.index.getName());
+		if (o.is_paired_end)
+		{
+			commands.add(FIRST_PAIR_OPTION);
+			commands.add(o.reads.get(0).reads.getAbsolutePath());
+			commands.add(SECOND_PAIR_OPTION);
+			commands.add(o.reads.get(1).reads.getAbsolutePath());
+		}
+		else
+		{
+			commands.add(o.reads.get(0).reads.getAbsolutePath());
+		}
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		pb.directory(o.index.getParentFile());
+		try
+		{
+			Process p = pb.start();
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			String line = null;
+			while ((line = stdout.readLine()) != null)
+			{
+				System.out.printf("%03d: %s%n", index, line);
+			}
+			while ((line = stderr.readLine()) != null)
+			{
+				System.err.printf("%03d: %s%n", index, line);
+			}
+			p.waitFor();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void postAlignmentProcessing()
 	{
-		// TODO Auto-generated method stub
 	}
 
 	/**
