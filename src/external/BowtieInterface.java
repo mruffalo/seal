b4b86833cb73830b6e8d4b2f3e333f09bf89e9fc
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-10-08 12:47:30 -0400

    BowtieInterface: implement index creation

--- src/external/BowtieInterface.java
+++ src/external/BowtieInterface.java
@@ -1,5 +1,10 @@
 package external;
 
+import io.FastaWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,12 +24,57 @@ public class BowtieInterface extends AlignmentToolInterface
 		super(index_, description_, thresholds_, sequence_, list_, o_, m_);
 	}
 
+	private void createIndex()
+	{
+		String index_base_name = "index";
+		String index_addition = ".1.ebwt";
+		o.index = new File(o.genome.getParentFile(), index_base_name + index_addition);
+		if (o.index.isFile())
+		{
+			System.out.printf("%03d: %s%n", index, "Index found; skipping");
+		}
+		else
+		{
+			List<String> commands = new ArrayList<String>();
+			commands.add(BOWTIE_INDEX_COMMAND);
+			commands.add(o.genome.getAbsolutePath());
+			commands.add(index_base_name);
+			ProcessBuilder pb = new ProcessBuilder(commands);
+			pb.directory(o.index.getParentFile());
+			try
+			{
+				FastaWriter.writeSequence(sequence, o.genome);
+				Process p = pb.start();
+				BufferedReader stdout = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+				BufferedReader stderr = new BufferedReader(
+					new InputStreamReader(p.getErrorStream()));
+				String line = null;
+				while ((line = stdout.readLine()) != null)
+				{
+					System.out.printf("%03d: %s%n", index, line);
+				}
+				while ((line = stderr.readLine()) != null)
+				{
+					System.err.printf("%03d: %s%n", index, line);
+				}
+				p.waitFor();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
 	@Override
 	public void preAlignmentProcessing()
 	{
-		List<String> commands = new ArrayList<String>();
-		commands.add(BOWTIE_INDEX_COMMAND);
-
+		createIndex();
 	}
 
 	@Override
