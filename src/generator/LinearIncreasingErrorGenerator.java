f794a996fed88533b6c94e5ae42e9f89ee3d4c63
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-10-10 14:06:07 -0400

    LinearIncreasingErrorGenerator: start to implement this

--- src/generator/LinearIncreasingErrorGenerator.java
+++ src/generator/LinearIncreasingErrorGenerator.java
@@ -2,9 +2,49 @@ package generator;
 
 public class LinearIncreasingErrorGenerator extends FragmentErrorGenerator
 {
-	public LinearIncreasingErrorGenerator(String allowedCharacters)
+	private double beginErrorProbability;
+	private double endErrorProbability;
+
+	public LinearIncreasingErrorGenerator(String allowedCharacters_, double beginErrorProbability_,
+		double endErrorProbability_)
 	{
-		super(allowedCharacters);
+		super(allowedCharacters_);
+	}
+
+	public double getBeginErrorProbability()
+	{
+		return beginErrorProbability;
+	}
+
+	public void setBeginErrorProbability(double beginErrorProbability_)
+	{
+		if (beginErrorProbability_ <= 1.0 && beginErrorProbability_ >= 0.0)
+		{
+			beginErrorProbability = beginErrorProbability_;
+		}
+		else
+		{
+			// TODO: be nicer about this :)
+			throw new IllegalArgumentException("error probability must be >= 0.0 and <= 1.0");
+		}
+	}
+
+	public double getEndErrorProbability()
+	{
+		return endErrorProbability;
+	}
+
+	public void setEndErrorProbability(double endErrorProbability_)
+	{
+		if (endErrorProbability_ <= 1.0 && endErrorProbability_ >= 0.0)
+		{
+			endErrorProbability = endErrorProbability_;
+		}
+		else
+		{
+			// TODO: be nicer about this :)
+			throw new IllegalArgumentException("error probability must be >= 0.0 and <= 1.0");
+		}
 	}
 
 	@Override
