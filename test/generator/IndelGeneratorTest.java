9e0c5865caac6bdcc882ece0a49cf9c3efc0eb44
Matthew Ruffalo <matthew.ruffalo@case.edu>
2010-10-11 12:42:46 -0400

    Add IndelGeneratorTest

---
+++ test/generator/IndelGeneratorTest.java
@@ -0,0 +1,51 @@
+package generator;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Not really a unit test. Used for visually inspecting IndelGenerator's output.
+ * 
+ * @author mruffalo
+ */
+public class IndelGeneratorTest
+{
+	@Test
+	public void testGenerateErrorsCharSequence()
+	{
+		SeqGenSingleSequenceMultipleRepeats sg = new SeqGenSingleSequenceMultipleRepeats();
+		SequenceGenerator.Options sgo = new SequenceGenerator.Options();
+		sgo.length = 100;
+		CharSequence orig = sg.generateSequence(sgo);
+		FragmentErrorGenerator eg;
+
+		IndelGenerator.Options o;
+
+		o = new IndelGenerator.Options();
+		o.deleteLengthMean = 4;
+		o.deleteLengthStdDev = 0.5;
+		o.deleteProbability = 0.01;
+		eg = new IndelGenerator(SequenceGenerator.NUCLEOTIDES, o);
+		eg.setVerboseOutput(true);
+		eg.generateErrors(orig);
+
+		o = new IndelGenerator.Options();
+		o.insertLengthMean = 4;
+		o.insertLengthStdDev = 0.5;
+		o.insertProbability = 0.1;
+		eg = new IndelGenerator(SequenceGenerator.NUCLEOTIDES, o);
+		eg.setVerboseOutput(true);
+		eg.generateErrors(orig);
+
+		o = new IndelGenerator.Options();
+		o.deleteLengthMean = 4;
+		o.deleteLengthStdDev = 0.5;
+		o.deleteProbability = 0.01;
+		o.insertLengthMean = 4;
+		o.insertLengthStdDev = 0.5;
+		o.insertProbability = 0.1;
+		eg = new IndelGenerator(SequenceGenerator.NUCLEOTIDES, o);
+		eg.setVerboseOutput(true);
+		eg.generateErrors(orig);
+	}
+}
