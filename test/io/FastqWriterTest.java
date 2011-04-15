9293269bf3f0d406c6191a39f80572fa97e4412d
Matthew Ruffalo <matthew.ruffalo@case.edu>
2011-04-15 16:02:17 -0400

    Add empty FastqWriterTest

---
+++ test/io/FastqWriterTest.java
@@ -0,0 +1,17 @@
+package io;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FastqWriterTest
+{
+	/**
+	 * Doesn't test anything automatically; writes stuff to a file for manual
+	 * inspection
+	 */
+	@Test
+	public void testWriteFragments()
+	{
+		fail("Not yet implemented");
+	}
+}
