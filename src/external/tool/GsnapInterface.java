ee8ddb91d269ba55e50ff6c348d5eb1ce69c9b86
Matthew Ruffalo <matthew.ruffalo@case.edu>
2011-09-06 11:32:21 -0400

    GsnapInterface: add constructor

--- src/external/tool/GsnapInterface.java
+++ src/external/tool/GsnapInterface.java
@@ -1,9 +1,19 @@
 package external.tool;
 
+import external.AlignmentResults;
 import external.AlignmentToolInterface;
+
+import java.util.List;
+import java.util.Map;
 
 public class GsnapInterface extends AlignmentToolInterface
 {
+	public GsnapInterface(int index_, String description_, List<Integer> thresholds_,
+			Options o_, Map<String, AlignmentResults> m_)
+	{
+		super(index_, description_, thresholds_, o_, m_);
+	}
+
 	@Override
 	public void preAlignmentProcessing()
 	{
