3fe08593f59add1d06a095d12e363c80e0e99ae9
Matthew Ruffalo <matthew.ruffalo@case.edu>
2011-09-06 11:28:17 -0400

    Add empty GsnapInterface

---
+++ src/external/tool/GsnapInterface.java
@@ -0,0 +1,21 @@
+package external.tool;
+
+import external.AlignmentToolInterface;
+
+public class GsnapInterface extends AlignmentToolInterface
+{
+	@Override
+	public void preAlignmentProcessing()
+	{
+	}
+
+	@Override
+	public void align()
+	{
+	}
+
+	@Override
+	public void postAlignmentProcessing()
+	{
+	}
+}
