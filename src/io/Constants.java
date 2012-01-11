43f279c67808d14cbce1d29266fa3e291a537110
Matthew Ruffalo <matthew.ruffalo@case.edu>
2012-01-11 13:26:20 -0500

    io.Constants: add note about offsets
    It's been long enough that I confused myself with this,
    so I'm sure anyone else will have a hard time with it too

--- src/io/Constants.java
+++ src/io/Constants.java
@@ -7,6 +7,12 @@ public class Constants
 	public static final String FIELD_SEPARATOR = ",";
 	public static final Pattern READ_POSITION_HEADER = Pattern.compile("[>@]?\\d+:READ_POS=((\\d+)(,(\\d+))?)(/\\d+)?");
 	public static final String FASTQ_QUALITY_CHARACTERS = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+	/*
+	 * These offsets are not ASCII values; they are offsets
+	 * into FASTQ_QUALITY_CHARACTERS.
+	 *
+	 * (Subtract 33 from each of the values shown at http://en.wikipedia.org/wiki/Fastq .)
+	 */
 	public static final int SANGER_OFFSET = 0;
 	public static final int SOLEXA_OFFSET = 26;
 	public static final int ILLUMINA_1_3_OFFSET = 33;
